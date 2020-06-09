25e05db2798fcd5e4df12f9ff81f97b0cc5a300a
kongqiuqiang <kongqiuqiang@bytedance.com>
2020-06-09 16:05:47 +0800

    pedal metrics

--- pytorch/calculate_score_for_paper.py
+++ pytorch/calculate_score_for_paper.py
@@ -21,7 +21,7 @@ from inference import PianoTranscription
 
 
 def infer_prob(args):
-    """Inference output all waveforms of a evaluation dataset.
+    """Inference the output probabilites of MAESTRO dataset.
 
     Args:
       cuda: bool
@@ -30,21 +30,11 @@ def infer_prob(args):
 
     # Arugments & parameters
     workspace = args.workspace
-    filename = args.filename
-    split = args.split
     model_type = args.model_type
-    loss_type = args.loss_type
-    augmentation = args.augmentation
-    max_note_shift = args.max_note_shift
-    batch_size = args.batch_size
-    iteration = args.iteration
+    checkpoint_path = args.checkpoint_path
+    probs_dir = args.probs_dir
+    split = args.split
     device = torch.device('cuda') if args.cuda and torch.cuda.is_available() else torch.device('cpu')
-    
-    dataset = 'maestro'
-    # if args.maps_dataset:
-    #     suffix = '_maps'
-    # else:
-    #     suffix = ''
     
     sample_rate = config.sample_rate
     segment_seconds = config.segment_seconds
@@ -52,19 +42,11 @@ def infer_prob(args):
     frames_per_second = config.frames_per_second
     classes_num = config.classes_num
     begin_note = config.begin_note
+    dataset = 'maestro'
 
     # Paths
-    checkpoint_path = os.path.join(workspace, 'checkpoints', filename, 
-        model_type, 'loss_type={}'.format(loss_type), 
-        'augmentation={}'.format(augmentation), 'batch_size={}'.format(batch_size), 
-        '{}_iterations.pth'.format(iteration))
-
     hdf5s_dir = os.path.join(workspace, 'hdf5s', dataset)
 
-    probs_dir = os.path.join(workspace, 'probs', filename, dataset, 
-        'split={}'.format(split), model_type, 'loss_type={}'.format(loss_type), 
-        'augmentation={}'.format(augmentation), 'batch_size={}'.format(batch_size),
-        '{}_iterations'.format(iteration))
     create_folder(probs_dir)
 
     # Transcriptor
@@ -85,15 +67,17 @@ def infer_prob(args):
                 midi_events = [e.decode() for e in hf['midi_event'][:]]
                 midi_events_time = hf['midi_event_time'][:]
         
-                # Get targets
+                # Ground truths processor
                 target_processor = TargetProcessor(
                     segment_seconds=len(audio) / sample_rate, 
                     frames_per_second=frames_per_second, begin_note=begin_note, 
                     classes_num=classes_num)
 
-                (target_dict, note_events) = target_processor.process(
-                    start_time=0, midi_events_time=midi_events_time, 
-                    midi_events=midi_events, extend_pedal=True)
+                # Get ground truths
+                (target_dict, note_events, pedal_events) = \
+                    target_processor.process(start_time=0, 
+                        midi_events_time=midi_events_time, 
+                        midi_events=midi_events, extend_pedal=True)
 
                 ref_on_off_pairs = np.array([[event['onset_time'], event['offset_time']] for event in note_events])
                 ref_midi_notes = np.array([event['midi_note'] for event in note_events])
@@ -103,13 +87,18 @@ def infer_prob(args):
                 transcribed_dict = transcriptor.transcribe(audio, midi_path=None)
                 output_dict = transcribed_dict['output_dict']
 
-                # Data to save out
+                # Pack probabilites to dump
                 total_dict = {key: output_dict[key] for key in output_dict.keys()}
                 total_dict['frame_roll'] = target_dict['frame_roll']
                 total_dict['ref_on_off_pairs'] = ref_on_off_pairs
                 total_dict['ref_midi_notes'] = ref_midi_notes
                 total_dict['ref_velocity'] = ref_velocity
 
+                if 'pedal_frame_output' in output_dict.keys():
+                    total_dict['ref_pedal_on_off_pairs'] = \
+                        np.array([[event['onset_time'], event['offset_time']] for event in pedal_events])
+                    total_dict['pedal_frame_roll'] = target_dict['pedal_frame_roll']
+                    
                 prob_path = os.path.join(probs_dir, '{}.pkl'.format(get_filename(hdf5_path)))
                 create_folder(os.path.dirname(prob_path))
                 pickle.dump(total_dict, open(prob_path, 'wb'))
@@ -125,13 +114,14 @@ class RegressionScoreCalculator(object):
         self.frames_per_second = config.frames_per_second
         self.classes_num = config.classes_num
         self.velocity_scale = config.velocity_scale
-        self.velocity = True
+        self.velocity = False
+        self.pedal = True
 
         self.evaluate_frame = True
         self.onset_tolerance = 0.05
         self.offset_ratio = None
         self.offset_min_tolerance = 0.05
-        self.velocity = False
+        self.pedal_offset_threshold = 0.2
         
         (hdf5_names, self.hdf5_paths) = traverse_folder(hdf5s_dir)
 
@@ -150,7 +140,6 @@ class RegressionScoreCalculator(object):
         Args:
           params: list of float, thresholds
         """
-
         n = 0
         list_args = []
 
@@ -161,7 +150,8 @@ class RegressionScoreCalculator(object):
                     """e.g., [0, 'xx.h5', [0.3, 0.3, 0.3]]"""
            
         debug = False
-        if debug:     
+        if debug:
+            list_args = list_args[0 :] 
             for i in range(len(list_args)):
                 print(i)
                 self.calculate_score_per_song(list_args[i])
@@ -175,7 +165,7 @@ class RegressionScoreCalculator(object):
         stats_list = list(results)
         stats_dict = {}
         for key in stats_list[0].keys():
-            stats_dict[key] = [e[key] for e in stats_list]
+            stats_dict[key] = [e[key] for e in stats_list if key in e.keys()]
         
         return stats_dict
 
@@ -187,7 +177,7 @@ class RegressionScoreCalculator(object):
         """
         n = args[0]
         hdf5_path = args[1]
-        [onset_threshold, offset_threshod, frame_threshold] = args[2]
+        [onset_threshold, offset_threshold, frame_threshold] = args[2]
 
         return_dict = {}
 
@@ -216,14 +206,31 @@ class RegressionScoreCalculator(object):
         # Post processor
         post_processor = RegressionPostProcessor(self.frames_per_second, 
             classes_num=self.classes_num, onset_threshold=onset_threshold, 
-            offset_threshold=offset_threshod, 
-            frame_threshold=frame_threshold)
+            offset_threshold=offset_threshold, 
+            frame_threshold=frame_threshold, 
+            pedal_offset_threshold=self.pedal_offset_threshold)
 
-        output_dict = post_processor.get_onset_roll_from_regression(output_dict)
-        output_dict = post_processor.get_offset_roll_from_regression(output_dict)
+        # Calculate binarized onset output from regression output
+        (onset_output, onset_shift_output) = \
+            post_processor.get_binarized_output_from_regression(
+                reg_output=output_dict['reg_onset_output'], 
+                threshold=onset_threshold, neighbour=2)
+        output_dict['onset_output'] = onset_output
+        output_dict['onset_shift_output'] = onset_shift_output
+
+        # Calculate binarized offset output from regression output
+        (offset_output, offset_shift_output) = \
+            post_processor.get_binarized_output_from_regression(
+                reg_output=output_dict['reg_offset_output'], 
+                threshold=offset_threshold, neighbour=4)
+        output_dict['offset_output'] = offset_output
+        output_dict['offset_shift_output'] = offset_shift_output
 
         # Detect piano notes from output_dict
-        (est_on_off_vels, est_midi_notes) = post_processor.output_dict_to_midi_notes(output_dict)
+        est_on_off_note_vels = post_processor.output_dict_to_detected_notes(output_dict)
+        est_on_offs = est_on_off_note_vels[:, 0 : 2]
+        est_midi_notes = est_on_off_note_vels[:, 2]
+        est_vels = est_on_off_note_vels[:, 3] * self.velocity_scale
 
         # Calculate note metrics
         if self.velocity:
@@ -232,9 +239,9 @@ class RegressionScoreCalculator(object):
                        ref_intervals=ref_on_off_pairs,
                        ref_pitches=note_to_freq(ref_midi_notes),
                        ref_velocities=total_dict['ref_velocity'],
-                       est_intervals=est_on_off_vels[:, 0 : 2],
+                       est_intervals=est_on_offs,
                        est_pitches=note_to_freq(est_midi_notes),
-                       est_velocities=est_on_off_vels[:, 2] * velocity_scale,
+                       est_velocities=est_vels,
                        onset_tolerance=self.onset_tolerance, 
                        offset_ratio=self.offset_ratio, 
                        offset_min_tolerance=self.offset_min_tolerance))
@@ -243,13 +250,50 @@ class RegressionScoreCalculator(object):
                 mir_eval.transcription.precision_recall_f1_overlap(
                     ref_intervals=ref_on_off_pairs, 
                     ref_pitches=note_to_freq(ref_midi_notes), 
-                    est_intervals=est_on_off_vels[:, 0 : 2], 
+                    est_intervals=est_on_offs, 
                     est_pitches=note_to_freq(est_midi_notes), 
                     onset_tolerance=self.onset_tolerance, 
                     offset_ratio=self.offset_ratio, 
                     offset_min_tolerance=self.offset_min_tolerance)
 
-        print(note_f1)
+        if self.pedal:
+            # Calculate binarized pedal offset output from regression output
+            (pedal_offset_output, pedal_offset_shift_output) = \
+                post_processor.get_binarized_output_from_regression(
+                    reg_output=output_dict['reg_pedal_offset_output'], 
+                    threshold=self.pedal_offset_threshold, neighbour=4)
+            output_dict['pedal_offset_output'] = pedal_offset_output
+
+            # Detect piano notes from output_dict
+            ref_pedal_on_off_pairs = output_dict['ref_pedal_on_off_pairs']
+            est_pedal_on_offs = post_processor.output_dict_to_detected_pedals(output_dict)
+
+            # Calculate pedal metrics
+            if len(ref_pedal_on_off_pairs) > 0:
+                pedal_precision, pedal_recall, pedal_f1, _ = \
+                    mir_eval.transcription.precision_recall_f1_overlap(
+                        ref_intervals=ref_pedal_on_off_pairs, 
+                        ref_pitches=np.ones(ref_pedal_on_off_pairs.shape[0]), 
+                        est_intervals=est_pedal_on_offs, 
+                        est_pitches=np.ones(est_pedal_on_offs.shape[0]), 
+                        onset_tolerance=0.2)
+
+                return_dict['pedal_f1'] = pedal_f1
+
+                y_pred = (np.sign(total_dict['pedal_frame_output'] - 0.5) + 1) / 2
+                y_pred[np.where(y_pred==0.5)] = 0
+                y_true = total_dict['pedal_frame_roll']
+                y_pred = y_pred[0 : y_true.shape[0]]
+                y_true = y_true[0 : y_pred.shape[0]]
+                
+                tmp = metrics.precision_recall_fscore_support(y_true.flatten(), y_pred.flatten())
+                return_dict['pedal_frame_precision'] = tmp[0][1]
+                return_dict['pedal_frame_recall'] = tmp[1][1]
+                return_dict['pedal_frame_f1'] = tmp[2][1]
+
+                print('pedal f1: {:.3f}, frame f1: {:.3f}'.format(pedal_f1, return_dict['pedal_frame_f1']))
+
+        print('note f1: {:.3f}'.format(note_f1))
 
         return_dict['note_precision'] = note_precision
         return_dict['note_recall'] = note_recall
@@ -261,28 +305,12 @@ def calculate_metrics(args, thresholds=None):
 
     # Arugments & parameters
     workspace = args.workspace
-    filename = args.filename
+    probs_dir = args.probs_dir
     split = args.split
-    model_type = args.model_type
-    loss_type = args.loss_type
-    augmentation = args.augmentation
-    max_note_shift = args.max_note_shift
-    batch_size = args.batch_size
-    iteration = args.iteration
     dataset = 'maestro'
-
-    if args.maps_dataset:
-        suffix = '_maps'
-    else:
-        suffix = ''
     
     # Paths
     hdf5s_dir = os.path.join(workspace, 'hdf5s', dataset)
-
-    probs_dir = os.path.join(workspace, 'probs', filename, dataset, 
-        'split={}'.format(split), model_type, 'loss_type={}'.format(loss_type), 
-        'augmentation={}'.format(augmentation), 'batch_size={}'.format(batch_size),
-        '{}_iterations'.format(iteration))
 
     # Score calculator
     score_calculator = RegressionScoreCalculator(hdf5s_dir, probs_dir, split=split)
@@ -301,43 +329,21 @@ def calculate_metrics(args, thresholds=None):
 
 
 if __name__ == '__main__':
-
     parser = argparse.ArgumentParser(description='')
     subparsers = parser.add_subparsers(dest='mode')
     
     parser_infer_prob = subparsers.add_parser('infer_prob')
     parser_infer_prob.add_argument('--workspace', type=str, required=True)
-    parser_infer_prob.add_argument('--filename', type=str, required=True)
-    parser_infer_prob.add_argument('--split', type=str, required=True)
     parser_infer_prob.add_argument('--model_type', type=str, required=True)
-    parser_infer_prob.add_argument('--loss_type', type=str, required=True)
-    parser_infer_prob.add_argument('--augmentation', type=str, required=True)
-    parser_infer_prob.add_argument('--max_note_shift', type=int, required=True)
-    parser_infer_prob.add_argument('--batch_size', type=int, required=True)
-    parser_infer_prob.add_argument('--iteration', type=int, required=True)
+    parser_infer_prob.add_argument('--checkpoint_path', type=str, required=True)
+    parser_infer_prob.add_argument('--probs_dir', type=str, required=True)
+    parser_infer_prob.add_argument('--split', type=str, required=True)
     parser_infer_prob.add_argument('--cuda', action='store_true', default=False)
-    parser_infer_prob.add_argument('--maps_dataset', action='store_true', default=False)
 
     parser_metrics = subparsers.add_parser('calculate_metrics')
     parser_metrics.add_argument('--workspace', type=str, required=True)
-    parser_metrics.add_argument('--filename', type=str, required=True)
+    parser_metrics.add_argument('--probs_dir', type=str, required=True)
     parser_metrics.add_argument('--split', type=str, required=True)
-    parser_metrics.add_argument('--model_type', type=str, required=True)
-    parser_metrics.add_argument('--loss_type', type=str, required=True)
-    parser_metrics.add_argument('--augmentation', type=str, required=True)
-    parser_metrics.add_argument('--max_note_shift', type=int, required=True)
-    parser_metrics.add_argument('--batch_size', type=int, required=True)
-    parser_metrics.add_argument('--iteration', type=int, required=True)
-    parser_metrics.add_argument('--maps_dataset', action='store_true', default=False)
-
-    parser_new = subparsers.add_parser('evaluate_with_opt_thres')
-    parser_new.add_argument('--workspace', type=str, required=True)
-    parser_new.add_argument('--filename', type=str, required=True)
-    parser_new.add_argument('--model_type', type=str, required=True)
-    parser_new.add_argument('--loss_type', type=str, required=True)
-    parser_new.add_argument('--augmentation', type=str, required=True)
-    parser_new.add_argument('--batch_size', type=int, required=True)
-    parser_new.add_argument('--iteration', type=int, required=True)
 
     args = parser.parse_args()
 
@@ -346,9 +352,6 @@ if __name__ == '__main__':
 
     elif args.mode == 'calculate_metrics':
         calculate_metrics(args)
-
-    elif args.mode == 'evaluate_with_opt_thres':
-        evaluate_with_opt_thres(args)
 
     else:
         raise Exception('Incorrct argument!')
