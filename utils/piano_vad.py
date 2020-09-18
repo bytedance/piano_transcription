7d679549a1031974dd8ceae985380247ff00fa26
kongqiuqiang <kongqiuqiang@bytedance.com>
2020-09-18 11:10:33 +0800

    Add Google for comparison

--- utils/piano_vad.py
+++ utils/piano_vad.py
@@ -4,8 +4,8 @@ import numpy as np
 def note_detection_with_onset_offset_regress(frame_output, onset_output, 
     onset_shift_output, offset_output, offset_shift_output, velocity_output,
     frame_threshold):
-    """Estimate onset and offset, onset shift, offset shift and velocity of 
-    piano notes. First detect onsets with onset outputs, then detect offsets
+    """Process prediction matrices to note events information.
+    First, detect onsets with onset outputs. Then, detect offsets
     with frame and offset outputs.
     
     Args:
@@ -20,7 +20,7 @@ def note_detection_with_onset_offset_regress(frame_output, onset_output,
     Returns: 
       output_tuples: list of [bgn, fin, onset_shift, offset_shift, normalized_velocity], 
       e.g., [
-        [1821, 1909, 0.4749851, 0.3048533, 0.72119445], 
+        [1821, 1909, 0.47498, 0.3048533, 0.72119445], 
         [1909, 1947, 0.30730522, -0.45764327, 0.64200014], 
         ...]
     """
@@ -31,11 +31,13 @@ def note_detection_with_onset_offset_regress(frame_output, onset_output,
 
     for i in range(onset_output.shape[0]):
         if onset_output[i] == 1:
+            """Onset detected"""
             if bgn:
-                """Consecutive onsets"""
-                fin = i
+                """Consecutive onsets. E.g., pedal is not released, but two 
+                consecutive notes being played."""
+                fin = max(i - 1, 0)
                 output_tuples.append([bgn, fin, onset_shift_output[bgn], 
-                    offset_shift_output[fin], velocity_output[bgn]])
+                    0, velocity_output[bgn]])
                 frame_disappear, offset_occur = None, None
             bgn = i
 
@@ -75,7 +77,7 @@ def note_detection_with_onset_offset_regress(frame_output, onset_output,
 
 def pedal_detection_with_onset_offset_regress(frame_output, offset_output, 
     offset_shift_output, frame_threshold):
-    """Estimate onset and offset, onset shift and offset shift of pedals.
+    """Process prediction array to pedal events information.
     
     Args:
       frame_output: (frames_num,)
@@ -88,6 +90,100 @@ def pedal_detection_with_onset_offset_regress(frame_output, offset_output,
       e.g., [
         [1821, 1909, 0.4749851, 0.3048533], 
         [1909, 1947, 0.30730522, -0.45764327], 
+        ...]
+    """
+    output_tuples = []
+    bgn = None
+    frame_disappear = None
+    offset_occur = None
+
+    for i in range(1, frame_output.shape[0]):
+        if frame_output[i] >= frame_threshold and frame_output[i] > frame_output[i - 1]:
+            """Pedal onset detected"""
+            if bgn:
+                pass
+            else:
+                bgn = i
+
+        if bgn and i > bgn:
+            """If onset found, then search offset"""
+            if frame_output[i] <= frame_threshold and not frame_disappear:
+                """Frame disappear detected"""
+                frame_disappear = i
+
+            if offset_output[i] == 1 and not offset_occur:
+                """Offset detected"""
+                offset_occur = i
+
+            if offset_occur:
+                fin = offset_occur
+                output_tuples.append([bgn, fin, 0., offset_shift_output[fin]])
+                bgn, frame_disappear, offset_occur = None, None, None
+
+            if frame_disappear and i - frame_disappear >= 10:
+                """offset not detected but frame disappear"""
+                fin = frame_disappear
+                output_tuples.append([bgn, fin, 0., offset_shift_output[fin]])
+                bgn, frame_disappear, offset_occur = None, None, None
+
+    # Sort pairs by onsets
+    output_tuples.sort(key=lambda pair: pair[0])
+
+    return output_tuples
+
+
+###### Google's onsets and frames post processing. Only used for comparison ######
+def onsets_frames_note_detection(frame_output, onset_output, offset_output, 
+    velocity_output, threshold):
+    """Process pedal prediction matrices to note events information. onset_ouput 
+    is used to detect the presence of notes. frame_output is used to detect the 
+    offset of notes.
+    
+    Args:
+      frame_output: (frames_num,)
+      onset_output: (frames_num,)
+      threshold: float
+    
+    Returns: 
+      bgn_fin_pairs: list of [bgn, fin, velocity]. E.g. 
+        [[1821, 1909, 0.47498, 0.72119445], 
+         [1909, 1947, 0.30730522, 0.64200014], 
+         ...]
+    """
+    output_tuples = []
+
+    loct = None
+    for i in range(onset_output.shape[0]):
+        # Use onset_output is used to detect the presence of notes
+        if onset_output[i] > threshold:
+            if loct:
+                output_tuples.append([loct, i, velocity_output[loct]])
+            loct = i
+        if loct and i > loct:
+            # Use frame_output is used to detect the offset of notes
+            if frame_output[i] <= threshold:
+                output_tuples.append([loct, i, velocity_output[loct]])
+                loct = None
+
+    output_tuples.sort(key=lambda pair: pair[0])
+
+    return output_tuples
+
+
+def onsets_frames_pedal_detection(frame_output, offset_output, frame_threshold):
+    """Process pedal prediction matrices to pedal events information.
+    
+    Args:
+      frame_output: (frames_num,)
+      offset_output: (frames_num,)
+      offset_shift_output: (frames_num,)
+      frame_threshold: float
+
+    Returns: 
+      output_tuples: list of [bgn, fin], 
+      e.g., [
+        [1821, 1909], 
+        [1909, 1947], 
         ...]
     """
     output_tuples = []
@@ -114,13 +210,13 @@ def pedal_detection_with_onset_offset_regress(frame_output, offset_output,
 
             if offset_occur:
                 fin = offset_occur
-                output_tuples.append([bgn, fin, 0., offset_shift_output[fin]])
+                output_tuples.append([bgn, fin])
                 bgn, frame_disappear, offset_occur = None, None, None
 
             if frame_disappear and i - frame_disappear >= 10:
                 """offset not detected but frame disappear"""
                 fin = frame_disappear
-                output_tuples.append([bgn, fin, 0., offset_shift_output[fin]])
+                output_tuples.append([bgn, fin])
                 bgn, frame_disappear, offset_occur = None, None, None
 
     # Sort pairs by onsets
